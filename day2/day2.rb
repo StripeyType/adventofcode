a70013296c4e006fc8bb3f99464d3a3fd810a26d
James Scheffler <jscheffler@zipcar.com>
2015-12-09 15:35:57 -0500

    Correct Day 2 Puzzle 1 solution

--- day2/day2.rb
+++ day2/day2.rb
@@ -4,9 +4,9 @@ order_total = 0
 box_re = /(\d+)x(\d+)x(\d+)/
 $stdin.each_line do |ln|
   m = box_re.match(ln)
-  l = m[1]
-  w = m[2]
-  h = m[3]
+  l = m[1].to_i
+  w = m[2].to_i
+  h = m[3].to_i
   top   = l*w
   front = w*h
   side  = l*h
