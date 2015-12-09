cb3e6d45882b5c1ab6bba1fde9feb851365e6360
James Scheffler <jscheffler@zipcar.com>
2015-12-09 15:05:12 -0500

    first attempt at the solution for Day 2 Puzzle #1

---
+++ day2/day2.rb
@@ -0,0 +1,19 @@
+#!/bin/env ruby
+
+order_total = 0
+box_re = /(\d+)x(\d+)x(\d+)/
+$stdin.each_line do |ln|
+  m = box_re.match(ln)
+  l = m[1]
+  w = m[2]
+  h = m[3]
+  top   = l*w
+  front = w*h
+  side  = l*h
+
+  need = (2 * (top + front + side)) + [top,front,side].min
+  order_total += need 
+end
+
+puts
+puts order_total
